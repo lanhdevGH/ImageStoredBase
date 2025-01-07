c9c7f15a9be18d25521322155b8c3724251671a6
LanhNe <thanhlanhlit@gmail.com>
2025-01-07 11:03:19 +0700

    [api] create unit test

---
+++ test/ImageStoreBase.Api.UnitTest/Extensions/QueryableExtensions.cs
@@ -0,0 +1,12 @@
+﻿using ImageStoreBase.Api.UnitTest.Helpers;
+
+namespace ImageStoreBase.Api.UnitTest.Extensions
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<T> AsAsyncQueryable<T>(this IEnumerable<T> input)
+        {
+            return new NotInDbSet<T>(input);
+        }
+    }
+}
